3228bcc9abe1c66e4356a1eee34fb36f5933ec29
PengasKhan <cheesepuff42@gmail.com>
2023-11-10 00:27:04 -0600

    trim code and added comments
    removed unused constants and added comments that spell out the process our code uses.

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 const currentDayEl = $("#currentDay");
 const timeBlockEl = $(".time-block");
-const descriptionEl = $(".description");
-const hourEl = $(".hour");
 const rightNow = dayjs().format("dddd, MMMM DD YYYY");
-
+// our function is wrapped in a jQuery call
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   timeBlockEl.on("click", handleSaveEvent);
+
   function handleSaveEvent(event) {
     event.preventDefault();
 
+    // grabs the value of the child that is a textarea of .time-block
+    // this is our entered text in the time block
     const descriptionText = $(this).children("textarea").val();
 
+    // grabs parent id and sets as key for the text we store
     localStorage.setItem($(this).attr("id"), descriptionText);
   }
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  var curHour = dayjs().hour();
+  // creates a variable that contains the present hour
+  const curHour = dayjs().hour();
+
+  // Our for loop starts at 9 and ends at 17 to match the dictated time blocks
   for (var i = 9; i < 18; i++) {
+    // grabs the block by the id that matches loop value
+    // this is our current time block being selected by the for loop
     const parentId = $("#hour-" + i);
+    // grabs the child, like before, but leaves out .val for our second use of this constant
     const textArea = parentId.children("textarea");
+    // grabs the saved text in localStorage by key id that matches our loop value
     const data = localStorage.getItem("hour-" + i);
+    // populates our corresponding time block with our saved text
     textArea.val(data);
+    // sets the time class of our current block to correspond with it's relation to now
+    // we use the child variable so that the class we add will replace any old one
     if (curHour > i) {
       textArea.addClass("past");
     } else if (i > curHour) {
@@ -42,13 +40,11 @@ $(function () {
       textArea.addClass("present");
     }
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  // sets our time to current time
   function displayTime() {
     currentDayEl.text(rightNow);
   }
+
   displayTime();
 });
